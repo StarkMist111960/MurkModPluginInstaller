857df06ced9b04109cafc8c77ad81a5c4b0c7a72
StarkMist111960 <starkmist111960@gmail.com>
2025-11-13 12:12:15 -0700

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -5,4 +5,7 @@ echo "Installing PluginInstaller..."
 sleep 2 
 
 curl -O https://raw.githubusercontent.com/StarkMist111960/MurkModPluginInstaller/refs/heads/main/PluginInstaller.sh
+
+sleep 8
+
 chmod +x PluginInstaller.sh
