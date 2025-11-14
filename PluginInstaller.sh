94e0ab10f9d4143361c5feecc0927801a7821dcb
StarkMist111960 <starkmist111960@gmail.com>
2025-11-13 17:18:02 -0700

    Update PluginInstaller.sh

--- PluginInstaller.sh
+++ PluginInstaller.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# menu_plugin
+PLUGIN_NAME="PluginInstaller"
+PLUGIN_FUNCTION="Install new plugins"
+PLUGIN_AUTHOR="StarkMist111960"
+PLUGIN_VERSION="1.3"
 
 echo "Welcome, type the raw URL of the desired plugin"
 read -p "Enter URL: " URL
